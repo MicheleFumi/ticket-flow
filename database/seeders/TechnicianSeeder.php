4dfe08f22fa24032b530d1f80929b1bce835c091
feffe1702 <fpm.asd@gmail.com>
2025-05-18 23:20:54 +0200

    [WIP]

--- database/seeders/TechnicianSeeder.php
+++ database/seeders/TechnicianSeeder.php
@@ -8,15 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class TechnicianSeeder extends Seeder
 {
-    /**
-     * Esegui il seeder.
-     */
     public function run(): void
     {
         Technician::create([
             'name' => 'Tecnico di Prova',
             'email' => 'tecnico@example.com',
-            'password' => Hash::make('password123'), // Assicurati di cambiarlo in produzione
+            'password' => Hash::make('password123'),
         ]);
     }
 }
