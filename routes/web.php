f6f870729ed79476f3c8b6cce292ea8a64c73233
MicheleFumi <michele.fumi96@gmail.com>
2025-05-18 20:35:00 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ticketController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Auth.login');
+    return view('auth.login');
 });
 
 /* Route::get('/', function () {
