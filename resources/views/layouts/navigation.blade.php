61e01b681ecc200cc0c826335dc74b3cbdc77da4
MicheleFumi <michele.fumi96@gmail.com>
2025-05-20 21:11:29 +0200

    sistemato problema view mobile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,10 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                     <x-nav-link :href="route('technicians.index')" :active="request()->routeIs('technicians.index')">
+                    <x-nav-link :href="route('technicians.index')" :active="request()->routeIs('technicians.index')">
                         {{ __("Lista Tecnici") }}
                     </x-nav-link>
-                     <x-nav-link :href="route('tickets')" :active="request()->routeIs('tickets')">
+                    <x-nav-link :href="route('tickets')" :active="request()->routeIs('tickets')">
                         {{ __('Lista Ticket') }}
                     </x-nav-link>
                 </div>
@@ -30,7 +30,6 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -49,8 +48,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -76,6 +74,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('technicians.index')" :active="request()->routeIs('technicians.index')">
+                {{ __("Lista Tecnici") }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('tickets')" :active="request()->routeIs('tickets')">
+                {{ __('Lista Ticket') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
@@ -95,8 +99,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                            onclick="event.preventDefault(); this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
