ae106a1b28db818a0d605f12f42fb0d3477fd675
feffe1702 <fpm.asd@gmail.com>
2025-05-21 14:51:33 +0200

    [FIX]

--- resources/views/technicians/index.blade.php
+++ resources/views/technicians/index.blade.php
@@ -14,39 +14,41 @@
         </div>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 @container">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                @if(isset($technicians) && $technicians->count() > 0)
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead>
-                            <tr>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome Completo</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Disponibilità</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach($technicians as $technician)
+                <div class="overflow-x-auto">
+                    @if(isset($technicians) && $technicians->count() > 0)
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead>
                                 <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">{{ $technician->nome }} {{ $technician->cognome }}</td>
-                                    <td class="px-6 py-4 whitespace-nowrap">{{ $technician->email }}</td>
-                                    <td class="px-6 py-4 whitespace-nowrap ">
-                                        @if($technician->is_available === 1)
-                                            <span class="inline-block h-3 w-3 rounded-full bg-green-500" title="Disponibile"></span>
-                                        @else
-                                            <span class="inline-block h-3 w-3 rounded-full bg-red-500" title="Non Disponibile"></span>
-                                        @endif
-                                    </td>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome Completo</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Disponibilità</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                @else
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        {{ __("Nessun tecnico trovato.") }}
-                    </div>
-                @endif
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach($technicians as $technician)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ $technician->nome }} {{ $technician->cognome }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ $technician->email }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap ">
+                                            @if($technician->is_available === 1)
+                                                <span class="inline-block h-3 w-3 rounded-full bg-green-500" title="Disponibile"></span>
+                                            @else
+                                                <span class="inline-block h-3 w-3 rounded-full bg-red-500" title="Non Disponibile"></span>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            {{ __("Nessun tecnico trovato.") }}
+                        </div>
+                    @endif
+                </div>
                 <div class="p-2 text-gray-900 dark:text-gray-100">
 
                 </div>
