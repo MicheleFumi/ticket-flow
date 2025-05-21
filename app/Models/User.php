925f20a5a5ce1749ac0d5b7af55fbe9b8561ba8a
feffe1702 <fpm.asd@gmail.com>
2025-05-21 11:06:59 +0200

    [WIP] aggiunta funzione che converte un user in tecnico e destroy function per i tecnici

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
         'email',
         'password',
         'telefono',
+        'is_technician',
     ];
 
     protected $hidden = [
@@ -24,7 +25,7 @@ class User extends Authenticatable
     ];
 
     public function tickets()
-{
-    return $this->hasMany(Ticket::class)->orderBy('created_at');
-}
+    {
+        return $this->hasMany(Ticket::class)->orderBy('created_at');
+    }
 }
