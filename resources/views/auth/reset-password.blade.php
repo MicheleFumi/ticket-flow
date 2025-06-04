1a9926db2c5e3a27e12c157fed2c3b3f15154afc
feffe1702 <fpm.asd@gmail.com>
2025-06-04 18:28:07 +0200

    [WIP] fix rotta

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ $isTechnician ? route('password.store') : url('/api/reset-password') }}">
+    <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
         <!-- Password Reset Token -->
